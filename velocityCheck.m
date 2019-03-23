eb204c38f27d6a3b7c433f72adf1847f3b24cadc
Aman Sharma <aman.sharmarx@gmail.com>
2019-03-23 13:04:00 -0400

    fixed inequality sign

--- velocityCheck.m
+++ velocityCheck.m
@@ -1,9 +1,10 @@
 function ret = velocityCheck(vel1, vel2)
 
-ret = True;
-if (vel1 <= 0 && vel2 <=0)
-    if(abs(vel1) >= abs(vel2))
-        ret = False;
+ret = 1;
+
+if (vel1 >= 0 && vel2 >= 0)
+    if(abs(vel1) >= (vel2))
+        ret = 0;
     end
 end
 
