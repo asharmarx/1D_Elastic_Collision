a916a5f7b7de169b3f90f0804192343e9473b7d5
Aman Sharma <aman.sharmarx@gmail.com>
2019-03-24 20:29:28 -0400

    added more calculations

--- elastic_Collision.py
+++ elastic_Collision.py
@@ -19,17 +19,17 @@ collisionDirection = 'e';
 
 finalVelocityVec = initVelocityVec;
 
-def collisionwithBox(velocityVec):
-    finalVelocity1 = (2 * mass2 * initialVelocity2) / massConst1 + (initialVelocity1 * massConst3) / massConst1;
+def collisionwithBox(vel1, vel2):
+    finalVelocity1 = (2 * mass2 * vel2) / massConst1 + (vel1 * massConst3) / massConst1;
 
-    finalVelocity2 = (2 * mass1 * initialVelocity1) / massConst1 +
-    (initialVelocity2 * massConst2) / massConst1;
+    finalVelocity2 = (2 * mass1 * vel1) / massConst1 +
+    (vel2 * massConst2) / massConst1;
 
     return[finalVelocity1, finalVelocity2]
 
-def collisionwithWall(velocityVec):
+def collisionwithWall(vel1):
 
-    finalVelocity1 = -1*initialVelocity1;
+    finalVelocity1 = -1*vel1;
     return[finalVelocity1]
 
 
@@ -45,3 +45,19 @@ def velocityCheck(vel1, vel2):
 while (velocityCheck(initialVelocity1, initialVelocity2)):
 
     if  (collisionDirection == 'e'):
+        finalVelocity1, finalVelocity2 = collisionCount(initialVelocity1, initialVelocity1)
+
+        initialVelocity1 = finalVelocity1;
+        initialVelocity2 = finalVelocity2;
+
+        collisionDirection = 'w';
+
+    elif (collisionDirection == 'w'):
+
+        finalVelocity1 = -1*initialVelocity1;
+        initialVelocity1 = finalVelocity1;
+
+        collisionDirection = 'e';
+
+
+    finalVelocityVec = np.concatenate(finalVelocityVec, [finalVelocity1 finalVelocity2])
