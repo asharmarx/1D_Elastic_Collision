f6f6412e1b61d669936b69f6dd18490c690bff36
Aman Sharma <aman.sharmarx@gmail.com>
2019-03-25 00:23:08 -0400

    fixed some syntax erros

--- elastic_Collision.py
+++ elastic_Collision.py
@@ -4,7 +4,7 @@ import numpy as np
 mass1 = 1
 mass2 = mass1 * 1e2
 
-initVelocityVec = [0 -1]
+initVelocityVec = [0, -1]
 
 initialVelocity1 = initVelocityVec[0]
 initialVelocity2 = initVelocityVec[1]
@@ -20,16 +20,16 @@ collisionDirection = 'e'
 finalVelocityVec = initVelocityVec
 
 def collisionwithBox(vel1, vel2):
-    finalVelocity1 = (2 * mass2 * vel2) / massConst1 + (vel1 * massConst3) / massConst1
+    finalVelocity1 = (2 * mass2 * vel2) / massConst1 + (vel1 * massConst3) /massConst1
 
-    finalVelocity2 = (2 * mass1 * vel1) / massConst1 +
-    (vel2 * massConst2) / massConst1
+    finalVelocity2 = (2 * mass1 * vel1) / massConst1 + (vel2 * massConst2) / massConst1
 
     return[finalVelocity1, finalVelocity2]
 
 def collisionwithWall(vel1):
 
     finalVelocity1 = -1*vel1
+
     return[finalVelocity1]
 
 
@@ -37,15 +37,17 @@ def velocityCheck(vel1, vel2):
 
     ret = True
 
-    if (vel1 >= 0 && vel2 >= 0):
+    if (vel1 >= 0 and vel2 >= 0):
         if(abs(vel1) <= (vel2)):
             ret = False
+
+    return ret
 
 
 while (velocityCheck(initialVelocity1, initialVelocity2)):
 
     if  (collisionDirection == 'e'):
-        finalVelocity1, finalVelocity2 = collisionCount(initialVelocity1, initialVelocity1)
+        finalVelocity1, finalVelocity2 = collisionwithBox(initialVelocity1, initialVelocity2)
 
         initialVelocity1 = finalVelocity1
         initialVelocity2 = finalVelocity2
@@ -54,14 +56,15 @@ while (velocityCheck(initialVelocity1, initialVelocity2)):
 
     elif (collisionDirection == 'w'):
 
-        finalVelocity1 = -1*initialVelocity1
+        finalVelocity1 = collisionwithWall(initialVelocity1)
         initialVelocity1 = finalVelocity1
 
         collisionDirection = 'e'
 
 
-    finalVelocityVec = np.concatenate(finalVelocityVec, [finalVelocity1 finalVelocity2])
+    finalVelocityVec = np.concatenate((finalVelocityVec, [finalVelocity1, finalVelocity2]))
 
     collisionCount += 1
 
-    print(f"No. of Collisions: {collisionCount}")
+    print("No. of Collisions: {}\n" .format(collisionCount))
+    print("Vf1 = {0}\n Vf2 = {1}" .format(finalVelocity1, finalVelocity2))
