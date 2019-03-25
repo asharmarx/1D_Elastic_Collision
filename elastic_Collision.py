b4a529fec70590ed1961b6773ce25ad697982c2c
Aman Sharma <aman.sharmarx@gmail.com>
2019-03-24 23:47:55 -0400

    fixed semicolon typos

--- elastic_Collision.py
+++ elastic_Collision.py
@@ -1,45 +1,45 @@
-import matplotlib.plot as plt`
+import matplotlib.pyplot as plt
 import numpy as np
 
-mass1 = 1;
-mass2 = mass1 * 1e2;
+mass1 = 1
+mass2 = mass1 * 1e2
 
-initVelocityVec = [0 -1];
+initVelocityVec = [0 -1]
 
-initialVelocity1 = initVelocityVec[0];
-initialVelocity2 = initVelocityVec[1];
+initialVelocity1 = initVelocityVec[0]
+initialVelocity2 = initVelocityVec[1]
 
-massConst1 = mass1 + mass2;
-massConst2 = mass2 - mass1;
-massConst3 = mass1 - mass2;
+massConst1 = mass1 + mass2
+massConst2 = mass2 - mass1
+massConst3 = mass1 - mass2
 
-collisionCount = 0;
+collisionCount = 0
 
-collisionDirection = 'e';
+collisionDirection = 'e'
 
-finalVelocityVec = initVelocityVec;
+finalVelocityVec = initVelocityVec
 
 def collisionwithBox(vel1, vel2):
-    finalVelocity1 = (2 * mass2 * vel2) / massConst1 + (vel1 * massConst3) / massConst1;
+    finalVelocity1 = (2 * mass2 * vel2) / massConst1 + (vel1 * massConst3) / massConst1
 
     finalVelocity2 = (2 * mass1 * vel1) / massConst1 +
-    (vel2 * massConst2) / massConst1;
+    (vel2 * massConst2) / massConst1
 
     return[finalVelocity1, finalVelocity2]
 
 def collisionwithWall(vel1):
 
-    finalVelocity1 = -1*vel1;
+    finalVelocity1 = -1*vel1
     return[finalVelocity1]
 
 
 def velocityCheck(vel1, vel2):
 
-    ret = True;
+    ret = True
 
     if (vel1 >= 0 && vel2 >= 0):
         if(abs(vel1) <= (vel2)):
-            ret = False;
+            ret = False
 
 
 while (velocityCheck(initialVelocity1, initialVelocity2)):
@@ -47,17 +47,21 @@ while (velocityCheck(initialVelocity1, initialVelocity2)):
     if  (collisionDirection == 'e'):
         finalVelocity1, finalVelocity2 = collisionCount(initialVelocity1, initialVelocity1)
 
-        initialVelocity1 = finalVelocity1;
-        initialVelocity2 = finalVelocity2;
+        initialVelocity1 = finalVelocity1
+        initialVelocity2 = finalVelocity2
 
-        collisionDirection = 'w';
+        collisionDirection = 'w'
 
     elif (collisionDirection == 'w'):
 
-        finalVelocity1 = -1*initialVelocity1;
-        initialVelocity1 = finalVelocity1;
+        finalVelocity1 = -1*initialVelocity1
+        initialVelocity1 = finalVelocity1
 
-        collisionDirection = 'e';
+        collisionDirection = 'e'
 
 
     finalVelocityVec = np.concatenate(finalVelocityVec, [finalVelocity1 finalVelocity2])
+
+    collisionCount += 1
+
+    print(f"No. of Collisions: {collisionCount}")
